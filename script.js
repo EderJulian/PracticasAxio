3986b9f275e17667c8b4e944158535bb17cae57e
ederjulian <julianeder97@gmail.com>
2023-11-06 18:33:06 +0100

    done

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-// Variable global para almacenar los usuarios
+
 let users = [];
 
-// Función para obtener usuarios de la API y mostrar sus nombres en el DOM
+
 async function getUsersAndDisplay() {
     try {
-        // Realiza la petición Axios a la API JSONPlaceholder
+        
         const response = await axios.get('https://jsonplaceholder.typicode.com/users');
 
-        // Almacena los usuarios en la variable global
+        
         users = response.data;
 
-        // Obtiene el elemento del DOM donde se mostrarán los nombres
+        
         const userList = document.getElementById('userList');
 
-        // Limpia cualquier contenido previo
+        
         userList.innerHTML = '';
 
-        // Itera a través de los usuarios y muestra sus nombres en el DOM
+       
         users.forEach(user => {
             const userName = document.createElement('p');
             userName.textContent = user.name;
@@ -27,6 +27,5 @@ async function getUsersAndDisplay() {
     }
 }
 
-// Asocia la función a ejecutar al hacer clic en el botón
 const fetchUsersButton = document.getElementById('fetchUsersButton');
 fetchUsersButton.addEventListener('click', getUsersAndDisplay);
